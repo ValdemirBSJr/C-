26b4e39f8612ae7a1f23ec5353205c833ae594d8
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2019-10-29 11:23:32 -0300

    testes algoritmo C#
    testes algoritmo C#

---
+++ Configurando_o_APPCONFIG/Configurando_o_APPCONFIG/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration; //precisa desa classe adicionado tbm em referencia
+
+//Sempre verifique o arquivo de configuração do executável da aplicação. As alterações não são refletidas no arquivo de configuração do código fonte.
+
+namespace Configurando_o_APPCONFIG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool enviarEmail =
+    Convert.ToBoolean(ConfigurationManager.AppSettings["EnviarEmailDeErro"]);
+
+            if (enviarEmail)
+            {
+                Console.WriteLine("Enviar email - valor resgatado 'true'");
+            }
+
+            else
+            {
+                Console.WriteLine("Enviar email - valor resgatado 'false'");
+            }
+
+            
+
+            enviarEmail =
+                Convert.ToBoolean(ConfigurationManager.AppSettings["EnviarEmailDeErro"]);
+
+            Console.WriteLine("Enviar email = {0}", enviarEmail.ToString());
+
+            Configuration config =
+                ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["EnviarEmailDeErro"].Value = "false";
+            config.Save(ConfigurationSaveMode.Modified);
+
+            Console.WriteLine("\nConfiguração EnviarEmailDeErro alterado para 'false'.\n");
+
+            enviarEmail =
+                Convert.ToBoolean(ConfigurationManager.AppSettings["EnviarEmailDeErro"]);
+
+            Console.WriteLine("Enviar email = {0}\n", enviarEmail.ToString());
+
+            ConfigurationManager.RefreshSection("appSettings");
+
+            Console.WriteLine("Seção appSettings atualizada.\n");
+
+            enviarEmail =
+                Convert.ToBoolean(ConfigurationManager.AppSettings["EnviarEmailDeErro"]);
+
+            Console.WriteLine("Enviar email = {0}\n", enviarEmail.ToString());
+
+            config.AppSettings.Settings.Add("Quantidade", "12");
+
+            config.Save(ConfigurationSaveMode.Modified);
+
+            int quantidade =
+                Convert.ToInt32(config.AppSettings.Settings["Quantidade"].Value);
+
+            Console.WriteLine(quantidade);
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ cinco patinhos xuxa/cinco patinhos xuxa/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cinco_patinhos_xuxa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//inicio do codigo
+
+            //string cancaopatinhos;
+
+            for (int patinhos = 5; patinhos >= 1; patinhos--)
+            {
+                
+                
+                if (patinhos == 1)
+                {
+                    Console.WriteLine(Convert.ToString(patinhos) + " patinho foi passear, além das montanhas para brincar.");
+                    
+                    
+                    
+                }
+                else
+                {
+                    Console.WriteLine(Convert.ToString(patinhos) + " patinhos foram passear, além das montanhas para brincar.");
+                    
+                    
+                }
+
+              
+                Console.Out.Write("A mamãe gritou: ");
+
+                for (int quaqua = 5; quaqua > 1; quaqua--)
+                {
+                    Console.Write("quaquá ");
+                }
+                Console.WriteLine();
+
+                if (patinhos <= 1)
+                {
+                    
+                    Console.Out.WriteLine(" Mas só " + Convert.ToString(patinhos - 1) + " patinho voltou de lá");
+
+                    goto finalcancao;
+                }
+               
+                else
+                {
+
+                    Console.Out.WriteLine(" Mas só " + Convert.ToString(patinhos - 1) + " patinhos voltaram de lá");
+
+                }
+                
+
+            }
+        finalcancao:
+            Console.WriteLine("Mas nenhum patinho voltou de lá");
+
+           Console.WriteLine("A mamãe patinha foi procurar, Além das montanhas, Na beira do mar");
+
+Console.WriteLine("A mamãe gritou: Quá, quá, quá, quá");
+Console.WriteLine("E os cinco patinhos voltaram de lá.");
+
+            Console.ReadLine();
+
+           
+           
+
+
+
+        }//final do codigo
+    }
+}
