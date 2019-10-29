00d7378b295160e829a9872bd5385cac01f66fc3
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2019-10-29 12:10:11 -0300

    exemplo criar DLL C#
    exemplo criar DLL C#

---
+++ minhaDLL/minhaDLL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minhaDLL
+{
+    public class Class1
+    {
+        //Aqui declaro as propriedades quantas eu quiser
+
+        public string titulo, autor, editor;
+        public double preco;
+
+    
+
+    }
+}
