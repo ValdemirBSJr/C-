5164c8f4bebfd960cb308e4fef3ea12ea3f3ec89
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2019-10-29 11:50:58 -0300

    testes algoritmos C#
    testes algoritmos C#

---
+++ testaUsoBotoes/testaUsoBotoes/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testaUsoBotoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 27) //Tecla Esc na tabela ASCII
+
+            {
+                string texto = "TEXTO";
+                MessageBox.Show("TECLA ESC PRESSIONADA! VALOR DE VARIÁVEL MANTIDA: "+ texto);
+            }
+        }
+    }
+}
