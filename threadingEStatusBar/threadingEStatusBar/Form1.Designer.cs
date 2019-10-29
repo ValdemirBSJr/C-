34327445be045c68ea65ae7da1d5d9c7ab0414a8
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2019-10-29 12:24:26 -0300

    teste algoritmo C#
    teste algoritmo C#

---
+++ threadingEStatusBar/threadingEStatusBar/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace threadingEStatusBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.bgWorkerIndeterminado = new System.ComponentModel.BackgroundWorker();
+            this.btnRodarProcesso = new System.Windows.Forms.Button();
+            this.btnIndeterminado = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // bgWorkerIndeterminado
+            // 
+            this.bgWorkerIndeterminado.WorkerSupportsCancellation = true;
+            this.bgWorkerIndeterminado.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerIndeterminado_DoWork);
+            this.bgWorkerIndeterminado.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerIndeterminado_RunWorkerCompleted);
+            // 
+            // btnRodarProcesso
+            // 
+            this.btnRodarProcesso.Location = new System.Drawing.Point(32, 27);
+            this.btnRodarProcesso.Name = "btnRodarProcesso";
+            this.btnRodarProcesso.Size = new System.Drawing.Size(126, 34);
+            this.btnRodarProcesso.TabIndex = 0;
+            this.btnRodarProcesso.Text = "Rodar Processo";
+            this.btnRodarProcesso.UseVisualStyleBackColor = true;
+            this.btnRodarProcesso.Click += new System.EventHandler(this.btnRodarProcesso_Click);
+            // 
+            // btnIndeterminado
+            // 
+            this.btnIndeterminado.Location = new System.Drawing.Point(197, 27);
+            this.btnIndeterminado.Name = "btnIndeterminado";
+            this.btnIndeterminado.Size = new System.Drawing.Size(126, 34);
+            this.btnIndeterminado.TabIndex = 1;
+            this.btnIndeterminado.Text = "Rodar processo com tempo indeterminado";
+            this.btnIndeterminado.UseVisualStyleBackColor = true;
+            this.btnIndeterminado.Click += new System.EventHandler(this.btnIndeterminado_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(356, 27);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(126, 34);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(32, 83);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(450, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 118);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnIndeterminado);
+            this.Controls.Add(this.btnRodarProcesso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker bgWorkerIndeterminado;
+        private System.Windows.Forms.Button btnRodarProcesso;
+        private System.Windows.Forms.Button btnIndeterminado;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ threadingEStatusBar/threadingEStatusBar/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; //Classe pra usar o recurso  thread
+
+//NEsta caso, realizamos uma tarefa que irá ter fim com tempo determinado
+//Sabemos quantos passos serao executados. USaremos o backgroundworker1
+//Mudamos as propriedades  WorkerReportsProgress e WorkSupportCancellation para true
+
+    //para o método de tempo indeterminado a propriedade workersupportcancellation deve ficar como true workerreportprogress false
+
+    //caso queira desfazer as ações, usar o evento runworkercompleted
+
+namespace threadingEStatusBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRodarProcesso_Click(object sender, EventArgs e)
+        {
+            //desabilita os botoes enquanto a trafea e executada
+            //esse botoao rodar processo serve para processo com tempo determinado
+            btnIndeterminado.Enabled = false;
+            btnRodarProcesso.Enabled = false;
+
+            //define o estilo padrao do progressbar
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Value = 0;
+
+            //executa o processo de forma assincrona.
+            backgroundWorker1.RunWorkerAsync();
+
+            btnCancelar.Enabled = true;
+        }
+
+        //abaixo crio metodo que simula operação de tempo longo
+
+        private void TarefaLonga (int p)
+        {
+
+            for (int i = 0; i <= 10; i ++)
+            {
+                //Faz o thread dormir por 'p' milisegundos a cada passagem de loop
+                Thread.Sleep(p);
+
+                //o metodo beginInvoke é o que possibilita vc invokar uma tread diferente
+                //muito importante pois executa simultaneamente as threads
+                label2.BeginInvoke(
+                    new Action(() =>
+                    {
+                        label2.Text = "Tarefa: " + i.ToString() + " concluída.";
+                    }
+                    ));
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Aqui executaremos uma tarefa que realizar 100 passos
+            for (int i = 0; i < 100; i ++)
+            {
+                //a cada loop, para 1 milissegundo com a função
+                TarefaLonga(20);
+
+                //abaixo, incrementamos tambem o report progress do bgworker
+                this.backgroundWorker1.ReportProgress(i);
+
+                //Verifica se houve um pedido de cancelamento das tarefas
+                if (backgroundWorker1.CancellationPending)
+                {
+                    //se sim, para a tarefa da thread
+                    e.Cancel = true;
+
+                    //zera o percentual de progresso
+                    backgroundWorker1.ReportProgress(0);
+
+                    return;
+                }
+            }
+
+            //qundo terminar tudo finaliza o progresso
+            backgroundWorker1.ReportProgress(100);
+            
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //incrementa o valor da progressbar com o valor atual do progresso da tarefa
+            progressBar1.Value = e.ProgressPercentage;
+
+            //informa a porcentagem na forma de texto
+            label1.Text = e.ProgressPercentage.ToString() + "%.";
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                //se a operação for cancelada, informa ao usuário
+                label2.Text = "Operação cancelada pelo Usuário";
+            }
+            else if (e.Error !=null)
+            {
+                //informa ao usuario caso haja erro
+                label2.Text = "Um erro ocorreu durante o processo! Erro: "+ e.Error.ToString();
+            }
+            else
+            {
+                //informa que tarefa foi concluida com sucesso
+                label2.Text = "Tarefa concluída com sucesso!";
+            }
+
+            //habilita os botoes
+            btnRodarProcesso.Enabled = true;
+            btnIndeterminado.Enabled = true;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            //cancelamento da tarefa com fim determinado 'backgroundworker1'
+
+            if (backgroundWorker1.IsBusy) //se ele estiver em funcionamento
+            {
+                //notifica a tread que o cancelamento foi solicitado
+                //cancela a tarefa doWork dela
+                backgroundWorker1.CancelAsync();
+            }
+
+            if(bgWorkerIndeterminado.IsBusy)
+            {
+                //notifica a thread que o cancelamento foi solicitado
+                //cancela a tarefa doWork
+                bgWorkerIndeterminado.CancelAsync();
+            }
+
+            //desabilita o botao cancelar
+            btnCancelar.Enabled = false;
+            label1.Text = "Cancelando...";
+        }
+
+        private void bgWorkerIndeterminado_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //executa a tarefa
+            TarefaLonga(100);
+            
+            //Abaixo verifico se houve uma requisicao para cancelar a operacao
+
+            if(bgWorkerIndeterminado.CancellationPending)
+            {
+                //se sim, define  a propriedade cancel para true
+                //para que o evento workerComplete saiba que a tarefa foi cancelada
+                e.Cancel = true;
+                return;
+            }
+
+            //executa a tarefa pela segunda vez
+            //fiz isso para exemplificar que uma vez começada uma tarefa
+            //ela nao pode ser cancelada. caso seja solicitado, ela deixa de fazer as demais, nao aquela que ja comecou
+            TarefaLonga(500);
+
+            if (bgWorkerIndeterminado.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+        }
+
+        private void bgWorkerIndeterminado_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //caso seja cancelado...
+
+            if (e.Cancelled)
+            {
+                //reconfigura  a progressbar para o padrao
+                progressBar1.MarqueeAnimationSpeed = 0;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 0;
+
+                //caso a operacao seja cancelada, informa ao usuario
+                label2.Text = "Operação cancelada pelo usuário!";
+
+                //habilita o botao cancelar
+                btnCancelar.Enabled = true;
+
+                //limpa a label
+                label1.Text = String.Empty;
+            }
+            else if (e.Error != null)
+            {
+                //informa ao usuario do acontecimento que houve erro
+                label2.Text = "Aconteceu um erro durante a execucão do processo! Erro: " + e.Error.ToString();
+
+                //reconfigura  a progressbar para o padrao
+                progressBar1.MarqueeAnimationSpeed = 0;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 0;
+            }
+            else
+            {
+                //informa que a tarefa foi concluida com sucesso
+                label2.Text = "Tarefa concluída com sucesso!";
+
+                //carrega todo o progressvar
+                progressBar1.MarqueeAnimationSpeed = 0;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 100;
+                label1.Text = progressBar1.Value.ToString() + "%";
+            }
+
+            //habilita os botoes
+            btnRodarProcesso.Enabled = true;
+            btnIndeterminado.Enabled = true;
+        }
+
+        private void btnIndeterminado_Click(object sender, EventArgs e)
+        {
+            //desabiita os botoes
+            btnIndeterminado.Enabled = false;
+            btnRodarProcesso.Enabled = false;
+
+            bgWorkerIndeterminado.RunWorkerAsync();
+
+            //define a progressbar para indeterminado
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 5;
+
+            //informa
+            label1.Text = "Processando...";
+            btnCancelar.Enabled = true;
+        }
+    }
+}
